b33c66880f0a05a4615fae870a8e80d9a5b8503b
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-10-26 14:19:26 +0530

    Updates to CLI help screen

--- app/index.js
+++ app/index.js
@@ -6,21 +6,27 @@ const initializeCLI = require('./cli');
 const cli = meow(
 	`
 	Usage
-	  $ abhijithvijayan-portfolio [input]
+	  $ abhijithvijayan-portfolio [input] [options]
 
-  Options
-    --generate  Lorem ipsum
+	Input
+		generate	Bootstraps the portfolio template
+		serve  		Serves the portfolio template locally
+
+	Options
+		-t, --token TEXT       GitHub token
+		-r, --repo TEXT	       repository name
+		-m, --message TEXT     commit message
+		-v, --version          Show the version and exit with code 0
 
   Examples
-    $ abhijithvijayan-portfolio --generate
+		$ abhijithvijayan-portfolio generate
 `,
 	{
 		flags: {
-			input: ['generate'],
+			input: ['generate', 'serve'],
 			boolean: ['version'],
 			string: ['token', 'repo', 'message'],
 			alias: {
-				g: 'generate',
 				r: 'repo',
 				t: 'token',
 				v: 'version',
