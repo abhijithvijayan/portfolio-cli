50428b31e32c510884971b3267de8254a4f00413
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-10-26 20:28:30 +0530

    update serve command spinner message

--- app/serve.js
+++ app/serve.js
@@ -6,16 +6,16 @@ const validateDependencyInstallation = require('./utils/install');
 const servePortfolioTemplate = async templateDir => {
 	await validateDependencyInstallation('yarn --version');
 
-	const installDepsSpinner = new Spinner('Installing dependencies in the background. Hold on...');
+	const installDepsSpinner = new Spinner('Installing packages. This might take a couple of minutes.');
 	installDepsSpinner.start();
 
 	try {
 		await execa('yarn', ['install']);
 	} catch (err) {
-		installDepsSpinner.fail(`Something went wrong. Couldn't install the dependencies!`);
+		installDepsSpinner.fail(`Something went wrong. Couldn't install packages!`);
 		throw err;
 	}
-	installDepsSpinner.succeed(`You're all set`);
+	installDepsSpinner.succeed(`Success!`);
 
 	// ToDo: Delete unwanted user-specific content & serve on port
 };
