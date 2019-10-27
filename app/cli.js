00748cffedb96f97efba2e13a3c56396a856c165
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-10-27 16:24:47 +0530

    move showInitialCommandsToUser to utils/displayMessages.js

--- app/cli.js
+++ app/cli.js
@@ -13,10 +13,10 @@ const Spinner = require('./utils/spinner');
 const currentDate = require('./utils/moment');
 const { writeFileAsync } = require('./utils/fs');
 const servePortfolioTemplate = require('./serve');
-const flashError = require('./utils/displayMessages');
 const argumentValidator = require('./utils/validate');
 const deleteStrayFilesAndFolders = require('./utils/delete');
 const validateDependencyInstallation = require('./utils/install');
+const { flashError, showInitialCommandsToUser } = require('./utils/displayMessages');
 
 const portfolioDir = 'portfolio';
 // eslint-disable-next-line prefer-const
@@ -35,33 +35,6 @@ const performInitialCommit = () => {
 	commands.forEach(command => {
 		return execa.sync('git', command.split('%'));
 	});
-};
-
-/**
- *  Shows next actions to user
- */
-const showInitialCommandsToUser = destination => {
-	console.log(chalk.default(`Initialized a git repository.`));
-	console.log();
-	console.log(
-		chalk.default(
-			`Success! Created ${portfolioDir} at ${destination}\nInside that directory, you can run several commands:`
-		)
-	);
-	console.log();
-	console.log(chalk.cyan.bold(`  yarn dev`));
-	console.log(chalk.default(`    Starts the development server.`));
-	console.log();
-	console.log(chalk.cyan.bold(`  yarn build`));
-	console.log(chalk.default(`    Bundles the app for production.`));
-	console.log();
-	console.log(chalk.cyan.bold(`  yarn export`));
-	console.log(chalk.default(`    Bundles the app into static files using Next.js static exports.`));
-	console.log();
-	console.log(chalk.default(`We suggest that you begin by typing:`));
-	console.log();
-	console.log(chalk.default(`  ${chalk.cyan.bold(`cd`)} ${portfolioDir}`));
-	console.log(chalk.cyan.bold(`  abhijithvijayan-portfolio serve`));
 };
 
 const writeConfigFile = async () => {
@@ -96,7 +69,7 @@ const fetchPortfolioTemplate = async () => {
 	deleteStrayFilesAndFolders();
 	writeConfigFile();
 	performInitialCommit();
-	showInitialCommandsToUser(destination);
+	showInitialCommandsToUser({ destination, portfolioDir });
 };
 
 /**
--- app/utils/displayMessages.js
+++ app/utils/displayMessages.js
@@ -8,4 +8,31 @@ const flashError = message => {
 	process.exit(1);
 };
 
-module.exports = flashError;
+/**
+ *  Shows next actions to user
+ */
+const showInitialCommandsToUser = ({ destination, portfolioDir }) => {
+	console.log(chalk.default(`Initialized a git repository.`));
+	console.log();
+	console.log(
+		chalk.default(
+			`Success! Created ${portfolioDir} at ${destination}\nInside that directory, you can run several commands:`
+		)
+	);
+	console.log();
+	console.log(chalk.cyan.bold(`  yarn dev`));
+	console.log(chalk.default(`    Starts the development server.`));
+	console.log();
+	console.log(chalk.cyan.bold(`  yarn build`));
+	console.log(chalk.default(`    Bundles the app for production.`));
+	console.log();
+	console.log(chalk.cyan.bold(`  yarn export`));
+	console.log(chalk.default(`    Bundles the app into static files using Next.js static exports.`));
+	console.log();
+	console.log(chalk.default(`We suggest that you begin by typing:`));
+	console.log();
+	console.log(chalk.default(`  ${chalk.cyan.bold(`cd`)} ${portfolioDir}`));
+	console.log(chalk.cyan.bold(`  abhijithvijayan-portfolio serve`));
+};
+
+module.exports = { flashError, showInitialCommandsToUser };
