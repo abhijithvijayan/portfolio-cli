61644c2ecf62ba64862673640a5b691131e3f096
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-10-27 08:34:39 +0530

    overwrite config/index.js generated with sample file

--- app/utils/delete.js
+++ app/utils/delete.js
@@ -1,23 +1,28 @@
 const fs = require('fs');
 const execa = require('execa');
 const chalk = require('chalk');
+
 const { isWin } = require('./os');
+const { readFileAsync, writeFileAsync } = require('./fs');
 
 const deleteStrayFiles = async () => {
 	const deleteCommand = isWin ? 'del' : 'rm';
 
 	// 1. delete .kodiak.toml
 	if (fs.existsSync('.kodiak.toml')) execa(`${deleteCommand} .kodiak.toml`, { shell: true });
-	// 2. delete CODE_OF_CONDUCT.md
+	// 2. delete CONTRIBUTING.md
 	if (fs.existsSync('CONTRIBUTING.md')) execa(`${deleteCommand} CONTRIBUTING.md`, { shell: true });
 	// 3. delete CODE_OF_CONDUCT.md
 	if (fs.existsSync('CODE_OF_CONDUCT.md')) execa(`${deleteCommand} CODE_OF_CONDUCT.md`, { shell: true });
-	// 4. delete public/icons
-	// 5. delete public/images
-	// 6. delete public/resume.pdf
-	// 7. update public/manifest.json
-	// 8. delete markdown/**/* except sample.md
-	// 9. delete/replace README.md
+	// 4.overwrite `config/index.js` with `config/sample.js`
+	if (fs.existsSync('config/index.js')) {
+		await writeFileAsync('config/index.js', await readFileAsync('config/sample.js'));
+	}
+	// update public/manifest.json
+	// delete/replace README.md
+	// delete markdown/**/* except sample.md
+	// delete public/images
+	// delete public/icons
 };
 
 module.exports = deleteStrayFiles;
