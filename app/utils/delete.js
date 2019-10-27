03a3e63ab5cdcafffe578e97e264eb2388071c06
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-10-27 08:52:58 +0530

    major: delete markdown/ files & public/images|icons files

--- app/utils/delete.js
+++ app/utils/delete.js
@@ -21,11 +21,35 @@ const deleteStrayFiles = async () => {
 	}
 	// 5. delete public/resume.pdf
 	if (fs.existsSync('public/resume.pdf')) execa(`${deleteCommand} public/resume.pdf`, { shell: true });
+	/**
+	 *  delete markdown/** except sample.md
+	 *  delete public/images
+	 *  delete public/icons
+	 */
+	await del([
+		'public/images/**',
+		'public/icons/**',
+		'markdown/home/**',
+		'markdown/about/**',
+		'markdown/experience/**',
+		'markdown/featured/**',
+		'markdown/projects/**',
+		'markdown/contact/**',
+		'!markdown/home',
+		'!markdown/home/sample.md',
+		'!markdown/about',
+		'!markdown/about/sample.md',
+		'!markdown/experience',
+		'!markdown/experience/sample.md',
+		'!markdown/featured',
+		'!markdown/featured/sample.md',
+		'!markdown/projects',
+		'!markdown/projects/sample.md',
+		'!markdown/contact',
+		'!markdown/contact/sample.md',
+	]);
 	// update public/manifest.json
 	// delete/replace README.md
-	// delete markdown/**/* except sample.md
-	// delete public/images
-	// delete public/icons
 };
 
 module.exports = deleteStrayFiles;
