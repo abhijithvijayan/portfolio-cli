af1cdd6227ac12b8bb5116b379dc60bf721d634a
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-01-23 21:09:02 +0530

    refactor: remove unwanted packages

--- app/utils/validate.js
+++ app/utils/validate.js
@@ -1,14 +1,12 @@
 const isObject = require('validate.io-object');
-const isString = require('validate.io-string-primitive');
-const isBoolean = require('validate.io-boolean-primitive');
 
-const cli = require('../cli');
+// const cli = require('../cli');
 
 /**
  *  CLI arguments validator
  */
 const argumentValidator = _options => {
-	const { options } = cli;
+	// const { options } = cli;
 
 	if (!isObject(_options)) {
 		return new TypeError(`invalid input argument. Options argument must be an object. Value: \`${_options}\`.`);
